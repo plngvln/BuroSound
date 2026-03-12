044e1321afeb6f88fc4168d5c1d2c97c6c0f6d69
maksim <msakunov@inbox.ru>
2026-03-12 14:54:28 +0300

    mixin fix

--- src/main/java/net/p4pingvin4ik/burosound/mixin/SoundSystemMixin.java
+++ src/main/java/net/p4pingvin4ik/burosound/mixin/SoundSystemMixin.java
@@ -6,20 +6,16 @@ import net.p4pingvin4ik.burosound.SoundDuckingManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(SoundSystem.class)
 public class SoundSystemMixin {
 
     @Inject(
-            method = "play(Lnet/minecraft/client/sound/SoundInstance;)Lnet/minecraft/client/sound/SoundSystem$PlayResult;",
+            method = "play(Lnet/minecraft/client/sound/SoundInstance;)V",
             at = @At("HEAD")
     )
-    private void onPlayVoid(SoundInstance sound, CallbackInfoReturnable<SoundSystem.PlayResult> cir) {
-        this.process(sound);
-    }
-
-    private void process(SoundInstance sound) {
+    private void onPlay(SoundInstance sound, CallbackInfo ci) {
         if (sound != null && sound.getId() != null) {
             if (sound.getId().getPath().contains("note_block")) {
                 SoundDuckingManager.notifyNoteBlock();

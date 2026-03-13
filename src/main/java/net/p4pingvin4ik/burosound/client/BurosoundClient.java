a47f01037940fd1b40de7069f98dbcde62c46c08
maksim <msakunov@inbox.ru>
2026-03-14 00:17:58 +0300

    added block_trigger

--- src/main/java/net/p4pingvin4ik/burosound/client/BurosoundClient.java
+++ src/main/java/net/p4pingvin4ik/burosound/client/BurosoundClient.java
@@ -1,15 +1,19 @@
 package net.p4pingvin4ik.burosound.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.registry.Registries;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.resource.ResourceType;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
 import net.p4pingvin4ik.burosound.*;
-import net.p4pingvin4ik.burosound.SoundJsonReader;
 
 import static net.p4pingvin4ik.burosound.Burosound.LOGGER;
 
@@ -25,6 +29,25 @@ public class BurosoundClient implements ClientModInitializer {
 
         BurosoundCommand.register();
         BoxRenderManager.register();
+
+        UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
+            if (world == null || !world.isClient() || player == null || hitResult == null) {
+                return ActionResult.PASS;
+            }
+
+            BlockPos pos = hitResult.getBlockPos();
+            if (pos == null) {
+                return ActionResult.PASS;
+            }
+
+            Identifier currentDim = world.getRegistryKey().getValue();
+            Identifier blockId = Registries.BLOCK.getId(world.getBlockState(pos).getBlock());
+
+            MinecraftClient client = MinecraftClient.getInstance();
+            BoxTriggerManager.handleBlockInteraction(client, currentDim, pos, blockId);
+
+            return ActionResult.PASS;
+        });
 
         ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
             SoundJsonReader.readSoundsConfig();

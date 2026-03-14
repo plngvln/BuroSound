94d989d47cfc2a3550a406e3018701cd396ab6ef
maksim <msakunov@inbox.ru>
2026-03-14 11:05:44 +0300

    changed TextLayerType to SEE_THROUGH

--- src/main/java/net/p4pingvin4ik/burosound/BoxRenderManager.java
+++ src/main/java/net/p4pingvin4ik/burosound/BoxRenderManager.java
@@ -155,7 +155,7 @@ public class BoxRenderManager {
                         false,
                         modelViewMatrix,
                         context.consumers(),
-                        TextRenderer.TextLayerType.NORMAL,
+                        TextRenderer.TextLayerType.SEE_THROUGH,
                         0,
                         0xF000F0
                 );
